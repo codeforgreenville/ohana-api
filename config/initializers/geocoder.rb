09f593a66580b4c7ce9775c79e70a3649cdc05ae
Dan <daniel.laurent.fields@gmail.com>
2014-07-01 22:16:26 -0400

    Added redis caching to geocoded data

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,10 @@
+require 'redis'
+
+REDIS = Redis.connect(url: ENV['REDISTOGO_URL'])
+
 Geocoder.configure(
   lookup: :google,
+  cache: REDIS,
   always_raise: [
     Geocoder::OverQueryLimitError,
     Geocoder::RequestDenied,
